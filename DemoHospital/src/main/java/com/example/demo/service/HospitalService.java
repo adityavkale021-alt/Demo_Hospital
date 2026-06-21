07a88574f064cc8abb299d2432c4306a9fb3a504
sawan <sawan@Aditya_kale>
2026-06-21 17:21:17 +0530

    third commit

---
+++ DemoHospital/src/main/java/com/example/demo/service/HospitalService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Hospital;
+
+public interface HospitalService {
+	void add(Hospital s);
+	List<Hospital>display();
+}
