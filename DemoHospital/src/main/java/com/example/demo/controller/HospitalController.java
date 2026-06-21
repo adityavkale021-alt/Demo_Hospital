cf5b3819bab07d212554392cc2a5f336b0e0cb2f
sawan <sawan@Aditya_kale>
2026-06-21 16:53:12 +0530

    second Commit

---
+++ DemoHospital/src/main/java/com/example/demo/controller/HospitalController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Hospital;
+import com.example.demo.service.HospitalService;
+
+@RestController
+
+public class HospitalController {
+	
+	@Autowired
+	private HospitalService ss;
+	
+	@GetMapping("/display")
+	public List<Hospital> disaplyHospitals(){
+		return ss.display();
+	}
+	
+	@PostMapping("/add")
+	public void addHospital(Hospital s) {
+		ss.add(s);
+	}
+
+}
